13a631c5a5134d54a50b94dfe01c1ebad7a7ed7f
Joshua Yi <joshuay1@asu.edu>
2020-02-27 19:56:59 -0700

    parts 2 and 3 of homework

--- cse360assign2/src/cse360assign2/SimpleList.java
+++ cse360assign2/src/cse360assign2/SimpleList.java
@@ -13,9 +13,14 @@ public class SimpleList
 	}
 	
 	public void add(int a) {
-		if(count == 10) {
-			list[9] = 0;
-			count = count - 1;
+		if(count == list.length - 1) {	
+			int size = (int)Math.floor(list.length * 1.5);
+			int temp[] = new int[size];
+			
+			for (int inx = 0; inx < list.length; inx++){
+			      temp[inx] = list[inx];
+			   }
+			   list = temp;
 		}
 		
 		if(count > 0) {
@@ -30,12 +35,34 @@ public class SimpleList
 	public void remove(int r) {
 		boolean found = false;
 		
+		if(count > 1) {
+			double reduce = count/(list.length - 1);
+			int space = 1;
+			
+			if(reduce > .25) {
+				
+				while(reduce > .25) {
+					reduce = count/(list.length - space);
+					space++;
+				}
+				
+				int temp[] = new int[list.length - space];
+				
+				for (int inx = 0; inx < list.length; inx++){
+				      temp[inx] = list[inx];
+				   }
+				   list = temp;
+			}
+		}
+		
 		for(int inx = 0; inx < count; inx++) {
+			
 			if(list[inx] == r) {
 				found = true;
 			}
 			
 			if(found) {
+				
 				for(int jinx = inx; jinx < count - 1; jinx++) {
 					list[jinx] = list[jinx + 1];
 					inx--;
@@ -46,6 +73,21 @@ public class SimpleList
 		}
 	}
 	
+	public void append(int c) {
+		if(count == list.length - 1) {	
+			
+			int size = (int)Math.floor(list.length * 1.5);
+			int temp[] = new int[size];
+			
+			for (int inx = 0; inx < list.length; inx++){
+			      temp[inx] = list[inx];
+			   }
+			   list = temp;
+		}
+		list[count] = c;
+		count++;
+	}
+	
 	public int count() {
 		return count; //this statement returns the current count of the array
 	}
@@ -54,6 +96,7 @@ public class SimpleList
 		String string = "";
 		
 		if(count != 0) {
+			
 			for(int inx = 0; inx < count; inx++) {
 				string = string + list[inx];
 
@@ -64,12 +107,42 @@ public class SimpleList
 		}
 		return string;
 	}
+	
 	public int search(int s) {
+		
 		for(int inx = 0; inx < count; inx++) {
+			
 			if(list[inx] == s) {
 				return inx;
 			}
 		}
 		return -1;
+	}
+	
+	public int first() {
+		if(list[0] == '\0') {
+			return -1;
+		}
+		return list[0];
+	}
+	
+	public int last() {
+		if(list[list.length - 1] == '\0') {
+			return -1;
+		}
+		return list[list.length - 1];
+	}
+	
+	public int size() {
+		int inx = 0;
+		int counter = 0;
+		int spaces = 0;
+		
+		while(list[inx] != '\0') {
+			counter++;
+			inx++;
+		}
+		spaces = list.length - counter;
+		return spaces;
 	}
 }
